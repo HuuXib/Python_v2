5dd9d689f00163a7523b91830eda29d211216c8f
HuuXib <113255630+HuuXib@users.noreply.github.com>
2025-09-29 18:44:54 +0200

    Delete machine_learning/program directory

--- machine_learning/program/convert.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-xlsx_file = 'tabela brak przesiadek.xlsx'
-df = pd.read_excel(xlsx_file, engine='openpyxl')
-
-
-csv_file = 'przesiadki.csv'
-df.dropna(axis=1, how='all').to_csv('output.csv', index=False)
